42c0a83d99dc020b8a815286a2040e60e930c0eb
suchitbaniya <60533985+suchitbaniya@users.noreply.github.com>
2023-06-24 00:10:20 +0545

    Create email-sender.js

---
+++ appscript-send-email/email-sender.js
@@ -0,0 +1,21 @@
+function sendThankYouEmail() {
+const formResponsesSheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+const dataRange = formResponsesSheet.getDataRange();
+const data = dataRange.getValues();
+
+
+const lastRowIndex = dataRange.getLastRow();
+const lastResponse = data[lastRowIndex - 1];
+
+
+const name = lastResponse[1];
+const email = lastResponse[2];
+
+
+const subject = "Thank you for submitting the form";
+const body = 'Dear ${name},\n\nThank you for submitting the form. We appreciate your participation.\n\nBest regards,\nSuchitBaniya';
+
+
+MailApp.sendEmail(email, subject, body);
+Logger.log(email, subject, body)
+}
